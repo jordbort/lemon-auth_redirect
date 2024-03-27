3a67c246aed48d4da7b486c7bea3b8c0ba60ffaa
jordbort <jordan.bortner@gmail.com>
2024-03-27 12:11:10 -0400

    Refine styling

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -1,13 +1,13 @@
 export default function Instructions() {
     return (
         <>
-            <p className='message'>You can give lemony_friend access to your Twitch account by clicking the button below.</p>
+            <p className='message'>Click the "Authorize" button to give lemony_friend access to your Twitch account.</p>
             <form action='https://id.twitch.tv/oauth2/authorize'>
                 <input type='hidden' name='response_type' value='code' />
                 <input type='hidden' name='client_id' value='gdjv4q6wmt34j3ajx6w8wdgrj1oyax' />
                 <input type='hidden' name='redirect_uri' value='https://lemony-friend.netlify.app' />
                 <input type='hidden' name='scope' value='channel:manage:polls channel:read:polls moderator:manage:announcements moderator:manage:shoutouts' />
-                <input className='button' type='submit' value='Authorize' />
+                <input className='bold button' type='submit' value='Authorize' />
             </form>
         </>
     )
