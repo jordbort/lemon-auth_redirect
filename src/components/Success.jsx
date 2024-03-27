2eeb744b284ab185ca5bf2e0c36e8f2014360c57
jordbort <jordan.bortner@gmail.com>
2024-03-27 12:33:53 -0400

    Make copy function async

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,11 +1,16 @@
 export default function Success(props) {
     const { code, scope } = props
 
+    async function copyCode() {
+        try { await navigator.clipboard.writeText(`!authorize ${code}`) }
+        catch (err) { console.error(`Failed to copy code:`, err) }
+    }
+
     return (
         <>
             <p className='bold message'>Please copy/paste the following into Twitch chat to complete authorization:</p>
             <p className='message'>!authorize {code}</p>
-            <button className='bold button' onClick={() => navigator.clipboard.writeText(`!authorize ${code}`)}>Copy</button>
+            <button className='bold button' onClick={copyCode}>Copy</button>
         </>
     )
 }
